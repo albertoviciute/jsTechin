a29c9b5e9f14b828a9128bd54dccf552384f2f57
albertoviciute <albertoviciutee@gmail.com>
2024-01-10 20:16:24 +0200

    triangle done

--- triangle.js
+++ triangle.js
@@ -1,26 +1,27 @@
-// Trikampio kraštinės (atskirai) surašytos į masyvą.
-// Turime 5 trikmapius.
-let borderOne = [5, 6, 8, 7, 9]; 
-let borderTwo = [8, 6, 5, -7, 4]; 
-let borderThree = [10, -2, 5, 4, 7]
-
 function calcTriangleArea(borderOne, borderTwo, borderThree){
     let p = (borderOne + borderTwo + borderThree)/2;
     return Math.sqrt(p * (p - borderOne) * (p - borderTwo) * (p - borderThree));
 }
 
+function isTrue(a,b,c){
+    return (b + c > a && a + c > b && a + b > c);
+}
+
+function printTriangleInfo(a, b, c, d) {
+    console.log(`Trikampis, kurio kraštinės a = ${a}, b = ${b} ir c = ${c}, ${typeof d === 'number' ? 'plotas yra:' + d : 'neegzistuoja'}`);
+}
+
+let borderOne = [5, 6, 8, 7, 9]; 
+let borderTwo = [8, 6, 5, -7, 4]; 
+let borderThree = [10, -2, 5, 4, 7]
 let area;
 
 for (let i = 0; i < borderOne.length; i++) {
     let isPossible = isTrue(borderOne[i], borderTwo[i], borderThree[i]);
     if (isPossible){
         area = calcTriangleArea(borderOne[i], borderTwo[i], borderThree[i]);
-        console.log(`Triangle ${i + 1}: Area = ${area}`);
+        printTriangleInfo(borderOne[i], borderTwo[i], borderThree[i], area);
     } else {
-        console.log('Impossible to create a triangle.');
+        printTriangleInfo(borderOne[i], borderTwo[i], borderThree[i]);
     }
-}
-
-function isTrue(a,b,c){
-    return (b + c > a && a + c > b && a + b > c);
 }
